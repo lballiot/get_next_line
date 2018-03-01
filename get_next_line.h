6188a7fd39ff1a3f0446983992ec7ae1022c5265
Lydie BALLIOT <lballiot@e2r7p19.42.fr>
2018-03-01 17:35:35 +0100

    Add gnl simple ok, bonus 1 failed

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: lballiot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 11:18:43 by lballiot          #+#    #+#             */
-/*   Updated: 2018/02/28 17:30:54 by lballiot         ###   ########.fr       */
+/*   Updated: 2018/03/01 17:31:02 by lballiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 10
+# define CHAR '\n'
+# define TO_FIND "\n"
 # include "libft/libft.h"
 # include <stdio.h>
 
